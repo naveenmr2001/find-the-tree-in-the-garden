4a0d84ae2e121bc14786825eb416e86f77df89c2
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-23 20:43:12 +0530

    Add. Add the logic to find a tree in a garden

--- src/FindATreeInGarden.java
+++ src/FindATreeInGarden.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class FindATreeInGarden {
+
+    public static String FunctionToCheckTile(int[][] doraGarden,int tileToCheck){
+
+        int eachTileValue = 1;
+
+        for(int iteratorRow = 0;iteratorRow<doraGarden.length;iteratorRow++){
+
+            for(int iteratorColumn = 0;iteratorColumn<doraGarden[iteratorRow].length;iteratorColumn++){
+
+                if(tileToCheck == eachTileValue){
+
+                    if(iteratorRow == 0 || iteratorColumn == 0 || iteratorColumn == 4){
+
+                        return "Yes";
+
+                    }
+
+                }
+
+                eachTileValue++;
+            }
+
+        }
+
+        return "No";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the row size: ");
+        int rowSize =  scan.nextInt();
+
+        System.out.println("Enter the column size");
+        int columnSize = scan.nextInt();
+
+        System.out.println("Enter the tile to check: ");
+        int tileToCheck = scan.nextInt();
+
+        int[][] doraGarden = new int[rowSize][columnSize];
+
+        String tileIsPresentOrNot = FunctionToCheckTile(doraGarden,tileToCheck);
+
+        System.out.println(tileIsPresentOrNot);
+
     }
 }
